c3fba1797f3291e8a2722960e39703516cd6eec9
kaipora12 <87929070+kaipora12@users.noreply.github.com>
2025-03-15 09:43:35 +0900

    feat:7-official-pages

--- src/components/ReactCarousel.jsx
+++ src/components/ReactCarousel.jsx
@@ -4,33 +4,38 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 const works = [
-  { src: "/images/ANOTHER-SKY-in-NZ.jpg", title: "WORK1" },
-  { src: "/images/comigsoon.jpg", title: "WORK2" },
-  { src: "/images/comigsoon.jpg", title: "WORK3" },
-  { src: "/images/comigsoon.jpg", title: "WORK4" },
+  { src: "/images/ANOTHER-SKY-in-NZ.jpg", title: "WORK1", link: "/topicks" },
+  { src: "/images/important.png", title: "WORK2", link: "/topicks2" },
+  { src: "/images/comigsoon.jpg", title: "WORK3", link: "/works/work3" },
+  { src: "/images/comigsoon.jpg", title: "WORK4", link: "/works/work4" },
 ];
 
 const Carousel = () => {
   return (
     <div className="carousel-container">
-      {/* <h2 className="carousel-title">Works</h2> */}
       <Swiper
         modules={[Pagination, Autoplay]}
         spaceBetween={20}
         slidesPerView={3}
         loop={true}
         loopAdditionalSlides={1}
-        pagination={{ clickable: true }} 
+        pagination={{ clickable: true }}
         autoplay={{ delay: 3000, disableOnInteraction: false }}
         breakpoints={{
-          640: { slidesPerView: 1 },
+          480: { slidesPerView: 1, spaceBetween: 10 },
           768: { slidesPerView: 2 },
           1024: { slidesPerView: 3 },
         }}
       >
         {works.map((work, index) => (
-          <SwiperSlide key={index} className="swiper-slide">
-            <img src={work.src} alt={work.title} className="carousel-image" />
+          <SwiperSlide key={index}>
+            <a href={work.link}>
+              <img
+                src={work.src}
+                alt={work.title}
+                className="carousel-image"
+              />
+            </a>
           </SwiperSlide>
         ))}
       </Swiper>
